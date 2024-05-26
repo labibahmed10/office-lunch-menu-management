e344ff6e8e5671fb50a70d4910cb99846f9e44a2
labibahmed10 <labib.ahmed.372@gmail.com>
2024-05-27 00:29:00 +0600

    feat: navbar added, logout as a user done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
+import { NavbarComponent } from "./components/Header/Navbar";
 import { SignIn } from "./pages/SignIn/SignIn";
-// import { SignUp } from "./pages/Signup/SignUp";
 
 function App() {
   return (
-    <section className="w-screen h-screen flex justify-center items-center">
-      <SignIn />
-
-      {/* <SignUp /> */}
-    </section>
+    <>
+      <NavbarComponent />
+      <section className="w-screen h-[calc(100vh-72px)] flex justify-center items-center">
+        <SignIn />
+      </section>
+    </>
   );
 }
 
