ccce5ed8dc4f6f265b331ce0b1e273e8de5110db
labibahmed10 <labib.ahmed.372@gmail.com>
2024-05-27 01:18:03 +0600

    feat:initial route manage setup done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import { NavbarComponent } from "./components/Header/Navbar";
-import { SignIn } from "./pages/SignIn/SignIn";
 
 function App() {
   return (
     <>
       <NavbarComponent />
-      <section className="w-screen h-[calc(100vh-72px)] flex justify-center items-center">
-        <SignIn />
-      </section>
+     
     </>
   );
 }
