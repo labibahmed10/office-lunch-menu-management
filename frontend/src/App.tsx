7f72849ae39c590fffdaf88b53d88e2a7c9a5e2f
labibahmed10 <labib.ahmed.372@gmail.com>
2024-05-27 20:01:54 +0600

    feat: added sidebar and dummy menu list as a card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
+import { Outlet } from "react-router-dom";
 import { NavbarComponent } from "./components/Header/Navbar";
+import SideBar from "./components/Sidebar/SideBar";
 
 function App() {
   return (
     <>
       <NavbarComponent />
-     
+      <section className="flex gap-3 w-full">
+        <SideBar />
+        <Outlet />
+      </section>
     </>
   );
 }
--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -2,11 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import { SignIn } from "../pages/SignIn/SignIn";
 import { SignUp } from "../pages/SignUp/SignUp";
 import App from "../App";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        path: "add-menu",
+        element: <Home />,
+      },
+    ],
   },
   {
     path: "/signin",
